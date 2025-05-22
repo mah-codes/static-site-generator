e300939d972e2f2be520b10dfff0ec47fe07a124
mah-codes <mathias.henderson7@gmail.com>
2025-05-22 17:56:19 -0300

    Fix: CH3L1 used split() method and added tests from solution

--- src/processing.py
+++ src/processing.py
@@ -2,30 +2,24 @@ from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     """Splits TextNode into list of TextNodes by MD delimiter"""
-    # Remember: delimiter is given already, hence always max 3 resulting nodes
-    # Segregate tgt text (incl delimiter) from TextNode
-    # check for delimiters within delimited text? recursion
+    # Lane has a check for non-closed sub-sections, raising a ValueError
+
+    
     new_nodes = []
     for node in old_nodes:
-        inner_delim_ind = node.text.find(delimiter)
-        outer_delim_ind = node.text.find(delimiter, inner_delim_ind + 1)
-        if inner_delim_ind < 0:
+        # Only TextType.TEXT will have 'embedded' TextTypes
+        if node.text_type != TextType.TEXT:
             new_nodes.append(node)
             continue
-        outer_node_1 = TextNode(
-            node.text[:inner_delim_ind],
-            TextType.TEXT,
-            url=None
-        )
-        inner_node = TextNode(
-            node.text[inner_delim_ind + 1:outer_delim_ind],
-            text_type,
-            url=None
-        )
-        outer_node_2 = TextNode(
-            node.text[outer_delim_ind + 1:],
-            TextType.TEXT,
-            url=None
-        )
-        new_nodes.extend([outer_node_1, inner_node, outer_node_2])
+        split_nodes = node.text.split(delimiter)
+        if len(split_nodes) % 2 == 0:
+            raise ValueError("includes non-closed delimiters")
+        for i in range(len(split_nodes)):
+            if split_nodes[i] == "":
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(split_nodes[i], TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(split_nodes[i], text_type))
+        # new_nodes.extend()
     return new_nodes
--- src/test_processing.py
+++ src/test_processing.py
@@ -13,3 +13,32 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             TextNode(" word", TextType.TEXT),
         ]    
         self.assertEqual(new_nodes, expected_nodes)
+    
+    def test_delim_bold_double(self):
+        node = TextNode(
+            "This is text with a **bolded** word and **another**", TextType.TEXT
+        )
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word and ", TextType.TEXT),
+                TextNode("another", TextType.BOLD),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_bold_and_italic(self):
+        node = TextNode("**bold** and _italic_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+        self.assertListEqual(
+            [
+                TextNode("bold", TextType.BOLD),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+            ],
+            new_nodes,
+        )
+
