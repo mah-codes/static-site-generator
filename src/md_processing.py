195cc79dd34004331d1506a23aee7a2b8885e1a8
mah-codes <mathias.henderson7@gmail.com>
2025-05-26 14:43:42 -0300

    Add: BlockType recogntion for core html tags CH4L1

--- src/md_processing.py
+++ src/md_processing.py
@@ -1,2 +1,28 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    paragraph = "paragraph"
+    heading = "heading"
+    code = "code"
+    quote = "quote"
+    unordered_list = "unordered_list"
+    ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown):
     return [block.strip() for block in markdown.split("\n\n")]
+
+def markdown_block_to_blocktype(md_block):
+    if re.match(r"^#{1,6}\s\w+", md_block):
+        return BlockType.heading
+    elif re.match(r"^```\w*```$", md_block):
+        return BlockType.code
+    elif re.match(r"^>\s\w+", md_block):
+        return BlockType.quote
+    elif re.match(r"^-+\s\w+", md_block):
+        return BlockType.unordered_list
+    elif re.match(r"^\d+\.\s\w+", md_block):
+        block_numbers = re.findall(r"(?m)^(\d+)\.\s.+", md_block)
+        if block_numbers == [str(i) for i in range(1, len(block_numbers) + 1)]:
+            return BlockType.ordered_list
+    return BlockType.paragraph
--- src/test_md_processing.py
+++ src/test_md_processing.py
@@ -22,3 +22,44 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_markdown_block_to_blocktype_heading(self):
+        md = "# This is a heading"
+        block_type = markdown_block_to_blocktype(md)
+        self.assertEqual(block_type, BlockType.heading)
+
+    def test_markdown_block_to_blocktype_paragraph(self):
+        md = "This is a paragraph"
+        block_type = markdown_block_to_blocktype(md)
+        self.assertEqual(block_type, BlockType.paragraph)
+
+    def test_markdown_block_to_blocktype_ordered_list(self):
+        md = "1. This is a list item\n2. This is another list item"
+        block_type = markdown_block_to_blocktype(md)
+        self.assertEqual(block_type, BlockType.ordered_list)
+
+    def test_markdown_block_to_blocktype_ordered_list_invalid(self):
+        md = "1. This is a list item\n2. This is another list item\n2. This is a third list item"
+        block_type = markdown_block_to_blocktype(md)
+        self.assertEqual(block_type, BlockType.paragraph)
+
+    def test_markdown_block_to_blocktype_mixed(self):
+        md = """
+# This is a heading
+
+This is a paragraph
+
+- This is a list
+- with items
+        """
+        block_types = []
+        for block in markdown_to_blocks(md):
+            block_types.append(markdown_block_to_blocktype(block))
+        self.assertListEqual(
+            block_types,
+            [
+                BlockType.heading,
+                BlockType.paragraph,
+                BlockType.unordered_list,
+            ],
+        )
